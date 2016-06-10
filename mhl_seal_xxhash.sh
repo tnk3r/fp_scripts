cf62140c4bd742cec214e858d50eef74662de44f
tink3r <abcfilm@gmail.com>
2016-06-10 16:31:47 -0700

    Added Dialog to completed mhl seal

--- mhl_seal_xxhash.sh
+++ mhl_seal_xxhash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+alert () {
+osascript -e 'tell app "System Events" to display dialog  "MHL Sealed!"'
+}
 
 cd $(dirname "$1")
-
-/usr/local/bin/mhl seal -t xxhash $(basename "$1") &
+/usr/local/bin/mhl seal -t xxhash $(basename "$1") && alert && exit 0 &
